ebb126a42769bf8793070fc339d6fc2fc7987123
mohammadne <mohammadne.dev@gmail.com>
2022-11-23 14:13:42 +0330

    feat: update controller

--- controllers/at_controller.go
+++ controllers/at_controller.go
@@ -18,19 +18,27 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"strings"
+	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/go-logr/logr"
 	jobv1alpha1 "github.com/mohammadne/job-operator/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // AtReconciler reconciles a At object
 type AtReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+
+	Log logr.Logger
 }
 
 //+kubebuilder:rbac:groups=job.example.com,resources=ats,verbs=get;list;watch;create;update;patch;delete
@@ -47,11 +55,138 @@ type AtReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *AtReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := r.Log.WithValues("at", req.NamespacedName)
+	logger.Info("=== Reconciling At")
 
-	// TODO(user): your logic here
+	instance := &jobv1alpha1.At{}
+	err := r.Get(context.TODO(), req.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// object not found, could have been deleted after
+			// reconcile request, hence don't requeue
+			return ctrl.Result{}, nil
+		}
+
+		// error reading the object, requeue the request
+		return ctrl.Result{}, err
+	}
+
+	// if no phase set, default to Pending
+	if instance.Status.Phase == "" {
+		instance.Status.Phase = jobv1alpha1.PhasePending
+	}
+
+	// state transition PENDING -> RUNNING -> DONE
+	switch instance.Status.Phase {
+	case jobv1alpha1.PhasePending:
+		logger.Info("Phase: PENDING")
+
+		diff, err := timeUntilSchedule(instance.Spec.Schedule)
+		if err != nil {
+			logger.Error(err, "Schedule parsing failure")
+
+			return ctrl.Result{}, err
+		}
+
+		logger.Info("Schedule parsing done", "Result", fmt.Sprintf("%v", diff))
+
+		if diff > 0 {
+			// not yet time to execute, wait until scheduled time
+			return ctrl.Result{RequeueAfter: diff * time.Second}, nil
+		}
+
+		logger.Info("It's time!", "Ready to execute", instance.Spec.Command)
+		// change state
+		instance.Status.Phase = jobv1alpha1.PhaseRunning
+	case jobv1alpha1.PhaseRunning:
+		logger.Info("Phase: RUNNING")
+
+		pod := newPodForCR(instance)
+		err := ctrl.SetControllerReference(instance, pod, r.Scheme)
+		if err != nil {
+			// requeue with error
+			return ctrl.Result{}, err
+		}
+
+		query := &corev1.Pod{}
+		// try to see if the pod already exists
+		err = r.Get(context.TODO(), req.NamespacedName, query)
+		if err != nil && errors.IsNotFound(err) {
+			// does not exist, create a pod
+			err = r.Create(context.TODO(), pod)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+
+			// Successfully created a Pod
+			logger.Info("Pod Created successfully", "name", pod.Name)
+			return ctrl.Result{}, nil
+		} else if err != nil {
+			// requeue with err
+			logger.Error(err, "cannot create pod")
+			return ctrl.Result{}, err
+		} else if query.Status.Phase == corev1.PodFailed ||
+			query.Status.Phase == corev1.PodSucceeded {
+			// pod already finished or errored out`
+			logger.Info("Container terminated", "reason", query.Status.Reason,
+				"message", query.Status.Message)
+			instance.Status.Phase = jobv1alpha1.PhaseDone
+		} else {
+			// don't requeue, it will happen automatically when
+			// pod status changes
+			return ctrl.Result{}, nil
+		}
+	case jobv1alpha1.PhaseDone:
+		logger.Info("Phase: DONE")
+		// reconcile without requeuing
+		return ctrl.Result{}, nil
+	default:
+		logger.Info("NOP")
+		return ctrl.Result{}, nil
+	}
+
+	// update status
+	err = r.Status().Update(context.TODO(), instance)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func timeUntilSchedule(schedule string) (time.Duration, error) {
+	now := time.Now().UTC()
+	layout := "2006-01-02T15:04:05Z"
+	scheduledTime, err := time.Parse(layout, schedule)
+	if err != nil {
+		return time.Duration(0), err
+	}
+
+	return scheduledTime.Sub(now), nil
+}
+
+func newPodForCR(cr *jobv1alpha1.At) *corev1.Pod {
+	labels := map[string]string{
+		"app": cr.Name,
+	}
+
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name,
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:    "busybox",
+					Image:   "busybox",
+					Command: strings.Split(cr.Spec.Command, " "),
+				},
+			},
+			RestartPolicy: corev1.RestartPolicyOnFailure,
+		},
+	}
 }
 
 // SetupWithManager sets up the controller with the Manager.
@@ -59,4 +194,15 @@ func (r *AtReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&jobv1alpha1.At{}).
 		Complete(r)
+
+	// err := ctrl.NewControllerManagedBy(mgr).
+	// 	For(&jobv1alpha1.At{}).
+	// 	Owns(&corev1.Pod{}).
+	// 	Complete(r)
+
+	// if err != nil {
+	// 	return err
+	// }
+
+	// return nil
 }
